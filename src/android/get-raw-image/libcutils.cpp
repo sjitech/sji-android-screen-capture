ba265479745ed06808f73983676259a6233eac06
QianJin2013 <osexp2003@gmail.com>
2014-11-05 17:03:07 +0900

    add some utility definition

--- src/android/get-raw-image/libcutils.cpp
+++ src/android/get-raw-image/libcutils.cpp
@@ -3,3 +3,4 @@
 int32_t android_atomic_inc(volatile int32_t* addr) {}
 int32_t android_atomic_dec(volatile int32_t* addr) {}
 int android_atomic_release_cas(int32_t oldvalue, int32_t newvalue, volatile int32_t* addr) {}
+int property_get(const char *key, char *value, const char *default_value) {}
--- src/android/get-raw-image/libcutils.h
+++ src/android/get-raw-image/libcutils.h
@@ -8,6 +8,7 @@ extern "C" {
     int32_t android_atomic_dec(volatile int32_t* addr);
     int android_atomic_release_cas(int32_t oldvalue, int32_t newvalue, volatile int32_t* addr);
 	#define android_atomic_cmpxchg android_atomic_release_cas
+	int property_get(const char *key, char *value, const char *default_value);
 }
 
 #endif //end of lib
