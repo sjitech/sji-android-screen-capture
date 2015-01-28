cab2832f569b0e1521eb84e67fe875dfe0f918b6
QianJin2013 <osexp2003@gmail.com>
2015-01-28 19:52:58 +0900

    remove unnecessary code: use android itself as host PC

--- bin/host.android/start.sh
+++
@@ -1,3 +0,0 @@
-chmod 700 ffmpeg node
-export PATH=$PATH:$PWD  #add ffmpeg to path
-./node ../asc.js ./config.json
--- src/host.android/ffmpeg/build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./0_make_ndk_toolchain.sh || exit 1
-./1_download_ffmpeg.sh || exit 1
-./1_download_libvpx.sh || exit 1
-./1_download_libx264.sh || exit 1
-./2_build_libvpx_armv7.sh || exit 1
-./3_build_libx264_armv7.sh || exit 1
-./9_build_ffmpeg_armv7.sh || exit 1
