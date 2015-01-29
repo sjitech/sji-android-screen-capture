4286121114fc90b85bbc60d447395a78f109b2cd
QianJin2013 <osexp2003@gmail.com>
2015-01-29 21:40:33 +0900

    encrypt dlopen error message to hide api name

--- src/android/get-raw-image/dlopen.c
+++ src/android/get-raw-image/dlopen.c
@@ -12,7 +12,13 @@ int main(int argc, char** argv){
             fprintf(stderr, "%s: OK\n", argv[i]);
             ok = 1;
         } else {
-            fprintf(stderr, "%s: %s\n", argv[i], dlerror());
+            char buf[1024] = {0};
+            int j;
+            snprintf(buf, sizeof(buf)-1, dlerror());
+            for( j = strlen(buf)-1; j >= 0; j--) {
+                buf[j] ^= 0x1F;
+            }
+            fprintf(stderr, "%s: %s\n", argv[i], buf);
         }
     }
     return ok ? 0 : -1;
