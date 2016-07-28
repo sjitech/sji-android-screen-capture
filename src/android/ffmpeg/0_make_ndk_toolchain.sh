96d666985902fb0fd41b0ef9aad7f349baf2c71b
q <osexp2003@gmail.com>
2016-07-28 14:39:34 +0900

    adopt new ndk

--- src/android/ffmpeg/0_make_ndk_toolchain.sh
+++ src/android/ffmpeg/0_make_ndk_toolchain.sh
@@ -5,6 +5,6 @@ ls -d ./std_toolchain && { echo ./std_toolchain already exist; exit 0; }
 
 printenv NDK_ROOT > /dev/null || { echo please export NDK_ROOT=root_dir_of_your_android_ndk; exit 1; }
 
-$NDK_ROOT/build/tools/make-standalone-toolchain.py --install-dir ./std_toolchain --arc arm --verbose
+$NDK_ROOT/build/tools/make_standalone_toolchain.py --install-dir ./std_toolchain --arc arm --verbose
 
 echo ""; echo ok; echo ""
